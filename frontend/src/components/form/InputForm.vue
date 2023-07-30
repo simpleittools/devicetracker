1089d7e4e8ca31e39819ab14381d7f78ca3cf8be
Ryan Mooney <wm.ryan.mooney@gmail.com>
2023-07-29 23:04:32 -0800

    create InputForm.vue. Add Search bar to ClientListView.vue

---
+++ frontend/src/components/form/InputForm.vue
@@ -0,0 +1,57 @@
+<script setup>
+import {ref, defineProps, watch, defineEmits} from 'vue'
+const props = defineProps([
+  "inputType",
+  "name",
+  "id",
+  "inputClass",
+  "modelValue",
+  "label",
+  "labelClass"
+])
+const inputModel = ref(props.modelValue)
+const emit = defineEmits()
+
+watch(() => props.modelValue, (newValue) => {
+  inputModel.value = newValue
+})
+
+watch(inputModel, (newValue) => {
+  emit('update:modelValue', newValue)
+})
+</script>
+
+<template>
+  <div class="field is-horizontal">
+    <div class="field-label is-normal">
+      <label
+        :for="id"
+        class="label"
+        :class="labelClass"
+      >
+        {{label}}
+      </label>
+    </div>
+    <div class="field-body">
+      <div class="field">
+        <div class="control">
+          <input
+            :type="inputType"
+            :name="name"
+            :id="id"
+            class="input"
+            :class="inputClass"
+            v-bind="$attrs"
+            v-model="inputModel"
+          >
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/views/ClientListView.vue
+++ frontend/src/views/ClientListView.vue
@@ -2,6 +2,9 @@
 
   import {ref} from "vue";
   import RadioButton from "@/components/form/RadioButton.vue";
+  import InputForm from "@/components/form/InputForm.vue";
+
+  const clientSearch = ref('')
 
   const clientsToShow = ref("active")
   const radioGroupName = "clients-list"
@@ -11,26 +14,44 @@
     {label: "All Clients", id: "all-clients", value: "all"}
 
   ]
+
+
 </script>
 
 <template>
-  <h1>This is the list of all clients, sorted alphabetically</h1>
-  <div class="control">
-    <RadioButton
-      v-for="option in checkBoxOptions"
-      :key="option.value"
-      :id="option.id"
-      :value="option.value"
-      :label="option.label"
-      :name="radioGroupName"
-      v-model="clientsToShow"
-    />
+  <div class="container">
+    <h1>This is the list of all clients, sorted alphabetically</h1>
+    <div class="control">
+      <RadioButton
+        v-for="option in checkBoxOptions"
+        :key="option.value"
+        :id="option.id"
+        :value="option.value"
+        :label="option.label"
+        :name="radioGroupName"
+        v-model="clientsToShow"
+      />
+    </div>
+
+    <div v-if="clientsToShow == 'active'">Active Clients List</div>
+    <div v-else-if="clientsToShow =='inactive'">Inactive Clients List</div>
+    <div v-else-if="clientsToShow == 'all'">All Clients List</div>
+
+    <div class="columns">
+      <div class="column is-9">
+        <InputForm
+          v-model="clientSearch"
+          type="text"
+          name="client-search"
+          id="client-search"
+          label="Search Client"
+        />
+      </div>
+      <div class="column is-3">
+        <button class="button is-primary">Search</button>
+      </div>
+    </div>
   </div>
-
-  <div v-if="clientsToShow == 'active'">Active Clients List</div>
-  <div v-else-if="clientsToShow =='inactive'">Inactive Clients List</div>
-  <div v-else-if="clientsToShow == 'all'">All Clients List</div>
-
 </template>
 
 <style scoped>
